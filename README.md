1c742b4298670d4ec927d99daed8ca66ad64dbe7
1024 <rk.hh@live.com>
2016-11-02 18:45:09 +0800

    Wodrs frequency count done

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # 5_lang_frequency
+Run lang_frequency.py passing path to text file as argument to find 10 most popular words in the text.
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,48 @@
+import re
+import sys
+
+
+def get_separator(sequence):
+    for sign in ["\+", "/", "=", "\."]:
+        if re.match("[\w-]+{}[\w-]+".format(sign), sequence):
+            return sign
 
 
 def load_data(filepath):
-    pass
+    with open(filepath) as text_file:
+        text = text_file.read()
+    return text
 
 
 def get_most_frequent_words(text):
-    pass
+    sequence_pattern = re.compile("\(?[\w-]+[,\\.!\?:;\)]?", flags=re.U)
+    double_sequence_pattern = re.compile("\(?[\w-]+[\+/=\.][\w-]+[,\\.!\?:;\)]?",
+                                                                    flags=re.U)
+    lines = text.split("\n")
+    sequences = []
+    for line in lines:
+        sequences.extend(line.split(" "))
+    words = []
+    for sequence in sequences:
+        if not re.match(".*\d.*", sequence):
+            if re.match(sequence_pattern, sequence):
+                words.append(re.sub("[,\\.!\?:;\)\(]", "", sequence).lower())
+            elif re.match(double_sequence_pattern, sequence):
+                words.extend(sequence.split(get_separator(re.sub(
+                    "[,\\.!\?:;\)\(]","",
+                    sequence).lower())))
+    counts_dict = {}
+    for word in words:
+        if word in counts_dict:
+            counts_dict[word] += 1
+        else:
+            counts_dict[word] = 1
+    return sorted(counts_dict.items(), key=lambda x: x[1], reverse=True)
 
 
 if __name__ == '__main__':
-    pass
+    filepath = sys.argv[1]
+    text = load_data(filepath)
+    counts = get_most_frequent_words(text)[:10]
+    for item in counts:
+        print("{}: {}".format(item[0], item[1]))
